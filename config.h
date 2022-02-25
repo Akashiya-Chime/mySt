fed194c6ba83a783fa68ae4d7946beddff9b6a49
Akashiya-Chime <akashiyachime@163.com>
2022-02-25 13:10:09 +0800

    put patches in order

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Font Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Cascadia Code::pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -75,6 +75,7 @@ static int bellvolume = 0;
 
 /* default TERM value */
 char *termname = "st-256color";
+
 
 /*
  * spaces per tab
